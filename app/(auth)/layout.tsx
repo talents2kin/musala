2265d5be62172cde803cc80857ed4e7c2e9dcf50
Tonny NSENGA <redaction@talents2kin.com>
2024-05-07 10:05:30 +0100

    Implement initial file and folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function RootLayout({
+ children,
+}:Readonly<{
+ children:React.ReactNode;
+}>){
+return(
+
+  <main>
+ 
+   {children}
+  </main>
+
+)
+
+}
+
+
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function RootLayout({
+ children,
+}:Readonly<{
+ children:React.ReactNode;
+}>){
+return(
+ <html lang="en">
+  <main>
+   SIDEBAR
+   {children}
+  </main>
+ </html>
+)
+
+}
+
+
